ea61fd27d041b699eb09d07a533b368d3ab96b25
prashantns13 <parasharamshinde70@gmail.com>
2024-07-12 15:31:41 +0530

    fifth

--- webapp/firstapp/models.py
+++ webapp/firstapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class insurance(models.Model):
+    age=models.FloatField()
+    sex=models.FloatField()
+    bmi=models.FloatField()
+    children=models.FloatField()
+    smoker=models.FloatField()
+    region=models.FloatField()
+    expenses=models.FloatField()
+
+    
